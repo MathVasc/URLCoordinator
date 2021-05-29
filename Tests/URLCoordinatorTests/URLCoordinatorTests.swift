6b9ae825cc1e96be5a83f175b07d3fe4a35b919d
Matheus de Vasconcelos <matheus.de.v.moura@accenture.com>
2021-05-29 00:09:17 -0300

    feat(start project): Remove test file and Docs from Project

--- Tests/URLCoordinatorTests/URLCoordinatorTests.swift
+++ Tests/URLCoordinatorTests/URLCoordinatorTests.swift
@@ -8,8 +8,4 @@ final class URLCoordinatorTests: XCTestCase {
         // results.
         XCTAssertEqual(URLCoordinator().text, "Hello, World!")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
